f65cf11dc1e7ccedf0b8de665a81528d9eda1b02
joapedrop <pereira.carlosdaniel@gmail.com>
2025-10-20 20:37:13 -0300

    20 de outubro

--- batalhanaval.java
+++ batalhanaval.java
@@ -140,6 +140,44 @@ public class BatalhaNaval {
         }
     }
 
+    public static int VerificaçãoAtirarTorpedoEspecial(int[][] navios, int[] torpedo, int[][] tabuleiro) {
+        int acertos = 0;
+        int centrolinha = torpedo[0];
+        int centocoluna = torpedo[1];
+
+        int[][] direçoes = { { 0, 0 }, { -1, 0 }, { 1, 0 }, { 0, -1 }, { 0, 1 } };
+
+        for (int i = 0; i < direçoes.length; i++) {
+            int linhaalvo = centrolinha + direçoes[i][0];
+            int colunaalvo = centocoluna + direçoes[i][1];
+
+            if (linhaalvo >= 0 && linhaalvo < 5 && colunaalvo >= 0 && colunaalvo < 5) {
+                boolean acertoualvo = false;
+
+                for (int navio = 0; navio < navios.length; navio++) {
+                    if (navios[navio][2] == 2 && linhaalvo == navios[navio][0] && colunaalvo == navios[navio][1]) {
+                        System.out.printf("Você acertou o torpedo especial em (%d,%d)\n", linhaalvo + 1,
+                                colunaalvo + 1);
+                        navios[navio][2] = 1;
+                        tabuleiro[linhaalvo][colunaalvo] = 1;
+                        acertos++;
+                        acertoualvo = true;
+                        break;
+                    }
+                }
+
+                if (!acertoualvo) {
+                    if (tabuleiro[linhaalvo][colunaalvo] != 1) {
+                        tabuleiro[linhaalvo][colunaalvo] = 0;
+                    }
+
+                }
+            }
+        }
+
+        return acertos;
+    }
+
     public static boolean Acertou(int[][] navios, int[] torpedo) {
         for (int navio = 0; navio < navios.length; navio++) {
             if (torpedo[0] == navios[navio][0] && torpedo[1] == navios[navio][1]) {
@@ -187,7 +225,7 @@ public class BatalhaNaval {
                 torpedo[0] + 1, linha, torpedo[1] + 1, coluna);
     }
 
-        public static void setColor(int cor) {
+    public static void setColor(int cor) {
         String s = "[0m";
         switch (cor) {
             case 0:
@@ -219,7 +257,16 @@ public class BatalhaNaval {
         System.out.print((char) 27 + s);
     }
 
+    public static boolean Ehtorpedonormal(int tentativas, int numeroaleatorio) {
+        if (tentativas == numeroaleatorio) {
+            return true;
+        }
+
+        return false;
+    }
+
     public static void main(String[] args) {
+        Random aleatorio = new Random();
         int qtddenavios = 3;
         int[][] tabuleiroJ1 = new int[5][5];
         int[][] tabuleiroJ2 = new int[5][5];
@@ -227,8 +274,6 @@ public class BatalhaNaval {
         int[][] naviosJ2 = new int[qtddenavios][3];
         int[] torpedoJ1 = new int[2];
         int[] torpedoJ2 = new int[2];
-        int[] torpedoemcruz1J = new int[5];
-        int[] torpedoemcruz2J = new int[5];
         int tentativasJ1 = 0;
         int tentativasJ2 = 0;
         int acertosJ1 = 0;
@@ -237,6 +282,10 @@ public class BatalhaNaval {
         String nomeJ2 = null;
         Set<String> torpedosatiradosJ1 = new HashSet<>();
         Set<String> torpedosatiradosJ2 = new HashSet<>();
+        int acertosrodadaJ1 = 0;
+        int acertosrodadaJ2 = 0;
+        int numeroaleatorioJ1 = aleatorio.nextInt(24) + 1;
+        int numeroaleatorioJ2 = aleatorio.nextInt(24) + 1;
 
         regras();
         setColor(-1);
@@ -253,61 +302,113 @@ public class BatalhaNaval {
         PosicionarNavios(naviosJ1, torpedosatiradosJ1, qtddenavios);
         PosicionarNavios(naviosJ2, torpedosatiradosJ2, qtddenavios);
 
-        do {
+        while (true) {
+            // TURNO DO JOGADOR 1
             System.out.println();
             System.out.printf("turno do %s\n", nomeJ1);
-
             imagemdotabuleiro(tabuleiroJ2, nomeJ1);
-            AtirarTorpedo(torpedoJ1, torpedosatiradosJ1);
-            tentativasJ1++;
+            boolean torpedonormal = false;
+            // VERIFICA SE O TORPEDO É NORMAL OU ESPECIAL(A CADA JOGO VAI TER UM UNICO
+            // TORPEDO ESPECIAL)
+            if (Ehtorpedonormal(tentativasJ1, numeroaleatorioJ1)) {
+                torpedonormal = true;
+            }
 
+            AtirarTorpedo(torpedoJ1, torpedosatiradosJ1);
             limparTela();
 
-            boolean acertouJ1 = Acertou(naviosJ2, torpedoJ1);
+            if (torpedonormal) {
+                // VERIFICA SE ACERTOU O NAVIO DO JOGADOR 2
+                boolean acertouJ1 = Acertou(naviosJ2, torpedoJ1);
+                // ALTERA O TABULEIRO DO JOGADOR 2 DE ACORDO COM O RESULTADO
+                AlterarTabuleiro(acertouJ1, tabuleiroJ2, torpedoJ1);
+
+                if (acertouJ1) {
+                    acertosrodadaJ1++;
+                }
+            } else {
+                System.out.println("parabens! %s você ganhou um torpedo especial nesta rodada!");
+                // ATIRA E VERIFICA OS ACERTOS DO TORPEDO ESPECIAL E MUDA O TABULEIRO DO JOGADOR
+                // 2
+                acertosrodadaJ1 = VerificaçãoAtirarTorpedoEspecial(naviosJ2, torpedoJ1, tabuleiroJ2);
+            }
+            ++tentativasJ1;
+            acertosJ1 += acertosrodadaJ1;
 
             if (tentativasJ1 % 3 == 0) {
+                // REPOSICIONA OS NAVIOS DO JOGADOR 2
+                System.out.printf("os navios do %s se mexeram...\n", nomeJ2);
                 PosicionarNavios(naviosJ2, torpedosatiradosJ1, qtddenavios);
             }
 
-            if (acertouJ1) {
-                acertosJ1++;
-                if (acertosJ1 != 3) {
-                    Dica(naviosJ2, torpedoJ1, tentativasJ1);
+            if (acertosrodadaJ1 > 0) {
+                System.out.printf("o %s acertou %d navio(s) nesta rodada\n", nomeJ1, acertosrodadaJ1);
+                if (acertosJ1 > 3) {
+                    // DÁ UMA DICA AO JOGADOR 1
+                    Dica(naviosJ2, torpedoJ1, tentativasJ2);
                 }
             } else {
-                Dica(naviosJ2, torpedoJ1, tentativasJ1);
+                Dica(naviosJ2, torpedoJ1, tentativasJ2);
             }
 
-            AlterarTabuleiro(acertouJ1, tabuleiroJ2, torpedoJ1);
+            if (acertosJ1 == 3) {
+                break;
+            }
 
+            // TURNO DO JOGADOR 2
             System.out.println();
             System.out.printf(" turno do %s\n", nomeJ2);
             imagemdotabuleiro(tabuleiroJ1, nomeJ2);
-            AtirarTorpedo(torpedoJ2, torpedosatiradosJ2);
-            tentativasJ2++;
+            torpedonormal = false;
+            // VERIFICA SE O TORPEDO É NORMAL OU ESPECIAL(A CADA JOGO VAI TER UM UNICO
+            // TORPEDO ESPECIAL)
+            if (Ehtorpedonormal(tentativasJ2, numeroaleatorioJ2)) {
+                torpedonormal = true;
+            }
 
+            AtirarTorpedo(torpedoJ2, torpedosatiradosJ2);
             limparTela();
 
-            boolean acertouJ2 = Acertou(naviosJ1, torpedoJ2);
+            if (torpedonormal) {
+                // VERIFICA SE ACERTOU O NAVIO DO JOGADOR 2
+                boolean acertouJ2 = Acertou(naviosJ1, torpedoJ2);
+                AlterarTabuleiro(acertouJ2, tabuleiroJ1, torpedoJ2);
+
+                if (acertouJ2) {
+                    acertosrodadaJ2++;
+                }
+            } else {
+                System.out.println("parabens! %s você ganhou um torpedo especial nesta rodada!");
+                // ATIRA E VERIFICA OS ACERTOS DO TORPEDO ESPECIAL E MUDA O TABULEIRO DO JOGADOR
+                // 1
+                acertosrodadaJ2 = VerificaçãoAtirarTorpedoEspecial(naviosJ1, torpedoJ2, tabuleiroJ1);
+            }
+            ++tentativasJ2;
+            acertosJ2 += acertosrodadaJ2;
 
             if (tentativasJ2 % 3 == 0) {
+                System.out.printf("os navios do %s se mexeram...\n", nomeJ1);
+                // REPOSICIONA OS NAVIOS DO JOGADOR 1
                 PosicionarNavios(naviosJ1, torpedosatiradosJ2, qtddenavios);
             }
 
-            if (acertouJ2) {
-                acertosJ2++;
-                if (acertosJ2 != 3) {
+            if (acertosrodadaJ2 > 0) {
+                System.out.printf("o %s acertou %d navio(s) nesta rodada\n", nomeJ2, acertosrodadaJ2);
+                if (acertosJ2 > 3) {
+                    // DÁ UMA DICA AO JOGADOR 2
                     Dica(naviosJ1, torpedoJ2, tentativasJ2);
                 }
             } else {
                 Dica(naviosJ1, torpedoJ2, tentativasJ2);
             }
 
-            AlterarTabuleiro(acertouJ2, tabuleiroJ1, torpedoJ2);
+            if (acertosJ2 == 3) {
+                break;
+            }
+        }
 
-        } while (acertosJ1 < 3 && acertosJ2 < 3);
-        System.out.println();
         System.out.println("Vencedor: ");
+        // DECLARA O VENCEDOR
         if (acertosJ1 == 3) {
             System.out.printf("%s venceu", nomeJ1);
         } else {
